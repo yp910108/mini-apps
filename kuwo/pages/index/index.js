673605a7af121e60b008d8618c9b561b2d922fac
yuanpeng <1548018679@qq.com>
2020-02-16 17:15:03 +0800

    微信小程序

---
+++ kuwo/pages/index/index.js
@@ -0,0 +1,48 @@
+Page({
+  data: {
+    imgUrls: [
+      './assets/swiper-01.jpg',
+      './assets/swiper-02.jpg',
+      './assets/swiper-03.jpg',
+      './assets/swiper-04.jpg',
+      './assets/swiper-05.jpg',
+      './assets/swiper-06.jpg',
+      './assets/swiper-07.jpg'
+    ],
+    indicatorDots: true,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    newsongs: [
+      {
+        img: './assets/song-01.jpg',
+        desc: '华语新歌推荐',
+        listenNum: 8538
+      },
+      {
+        img: './assets/song-02.jpg',
+        desc: '日韩新歌推荐',
+        listenNum: 11045
+      },
+      {
+        img: './assets/song-03.jpg',
+        desc: '欧美新歌推荐',
+        listenNum: 8333
+      }
+    ],
+    songlist: [
+      {
+        img: './assets/songlist-01.jpg',
+        desc: '未来过去，我只想见你'
+      },
+      {
+        img: './assets/songlist-02.jpg',
+        desc: '2019年度抖音热歌'
+      },
+      {
+        img: './assets/songlist-03.jpg',
+        desc: '唤醒周末 ｜ 至还没有睡醒的你'
+      }
+    ]
+  }
+})
